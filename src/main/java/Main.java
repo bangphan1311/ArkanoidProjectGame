1a68a314adfb74a62b53afe2553a7146e3d856b8
HP VICTUS <24021384@vnu.edu.vn>
2025-10-06 17:14:09 +0700

    paddle di chuyen trai phai theo dieu huong ban phim

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,30 @@
+import controller.paddleController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static final double SCENE_WIDTH = 600;
+    private static final double SCENE_HEIGHT = 690;
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Hello.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("My JavaFX App");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Hello.fxml"));
+        paddleController paddleController = new paddleController();
+        loader.setController(paddleController);
+
+        Parent root = loader.load();
+        Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+
         stage.setScene(scene);
+        stage.setTitle("Arkanoid Game");
+        stage.setResizable(false);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
---
+++ src/main/java/controller/paddleController.java
@@ -0,0 +1,48 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+
+public class paddleController {
+
+    @FXML
+    private Rectangle Paddle;
+
+    @FXML
+    private Pane gamePane;
+
+    private static final double PADDLE_SPEED = 15;
+
+    @FXML
+    public void initialize() {
+        setupKeyListeners();
+    }
+
+    private void setupKeyListeners() {
+        gamePane.setOnKeyPressed(event -> {
+            double currentX = Paddle.getLayoutX();
+            double paddleWidth = Paddle.getWidth();
+
+            if (event.getCode() == KeyCode.LEFT || event.getCode() == KeyCode.A) {
+                double newX = currentX - PADDLE_SPEED;
+                if (newX >= 0) {
+                    Paddle.setLayoutX(newX);
+                } else {
+                    Paddle.setLayoutX(0);
+                }
+            } else if (event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.D) {
+                double newX = currentX + PADDLE_SPEED;
+                if (newX + paddleWidth <= gamePane.getWidth()) {
+                    Paddle.setLayoutX(newX);
+                } else {
+                    Paddle.setLayoutX(gamePane.getWidth() - paddleWidth);
+                }
+            }
+        });
+
+        gamePane.setFocusTraversable(true);
+        gamePane.requestFocus();
+    }
+}
