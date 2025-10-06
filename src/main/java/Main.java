fd3f01f9e86792be4af7d0f78eb9ccdda1b45f5d
HP VICTUS <24021384@vnu.edu.vn>
2025-10-06 15:19:57 +0700

    Setup Maven + JavaFX environment

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Hello.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("My JavaFX App");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
