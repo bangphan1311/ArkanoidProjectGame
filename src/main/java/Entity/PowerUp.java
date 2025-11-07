dad297a0676f654945c1ed5cb9e82a73b439006d
HP VICTUS <24021384@vnu.edu.vn>
2025-11-07 10:33:04 +0700

    thêm power up cũng như cơ chế tính điểm ( cơ bản )

--- src/main/java/Entity/PowerUp.java
+++ src/main/java/Entity/PowerUp.java
@@ -1,4 +1,51 @@
 package Entity;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.Node;
+
+/**
+ * Lớp đại diện cho một vật phẩm (Power-up) rơi xuống.
+ * Nâng cấp để chứa "type" (loại vật phẩm).
+ */
 public class PowerUp {
+
+    private ImageView imageView;
+    private double speedY = 2.5; // Tốc độ rơi
+    private boolean isCollected = false;
+    private String type; // Loại vật phẩm: "BANANA", "SPEED_UP", "PADDLE_CHANGE"
+
+    public PowerUp(Image image, double x, double y, String type) {
+        this.imageView = new ImageView(image);
+        this.imageView.setLayoutX(x);
+        this.imageView.setLayoutY(y);
+        this.imageView.setFitWidth(30); // Bạn có thể đổi kích thước ở đây
+        this.imageView.setFitHeight(30);
+        this.type = type; // Lưu loại vật phẩm
+    }
+
+    public void update() {
+        imageView.setLayoutY(imageView.getLayoutY() + speedY);
+    }
+
+    public Node getShape() {
+        return imageView;
+    }
+
+    public boolean isOutOfScreen(double screenHeight) {
+        return imageView.getLayoutY() > screenHeight;
+    }
+
+    public void setCollected() {
+        this.isCollected = true;
+    }
+
+    public boolean isCollected() {
+        return isCollected;
+    }
+
+    // Hàm mới để BaseGameController biết cần làm gì
+    public String getType() {
+        return type;
+    }
 }
